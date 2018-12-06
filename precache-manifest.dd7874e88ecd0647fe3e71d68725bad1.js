1355db74fe3171110b2835cdf297be40a569812a
lf7817 <535536456@qq.com>
2018-12-06 22:26:23 +0800

    Updates

---
+++ precache-manifest.dd7874e88ecd0647fe3e71d68725bad1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tetris/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0277f3ed11c442e22cbb",
+    "url": "/tetris/static/js/runtime~main.0277f3ed.js"
+  },
+  {
+    "revision": "6ee591f109529d684064",
+    "url": "/tetris/static/js/main.6ee591f1.chunk.js"
+  },
+  {
+    "revision": "1f8ebf9ffbdb6406779f",
+    "url": "/tetris/static/js/1.1f8ebf9f.chunk.js"
+  },
+  {
+    "revision": "6ee591f109529d684064",
+    "url": "/tetris/static/css/main.d647e56b.chunk.css"
+  },
+  {
+    "revision": "8e1fd4442c31443e7bfc6086cf4e6cba",
+    "url": "/tetris/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1c869d4ff1d4aaf8f6371e0fd890b47e.js"
+  "/tetris/precache-manifest.dd7874e88ecd0647fe3e71d68725bad1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/tetris/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
