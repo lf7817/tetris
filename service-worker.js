9e2ffb8c5285319446d564b61fb30f79ed2c207c
lf7817 <535536456@qq.com>
2018-12-14 13:17:46 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("./precache-manifest.8144aff78d89e276df14d9d315b6e31a.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("./precache-manifest.e66b2c23400cb248fcbc19864b82e506.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 workbox.clientsClaim();
 
