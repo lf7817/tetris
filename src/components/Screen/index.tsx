aaff0be2f5bfb0f3c26467cfb7a096aea33e8182
lf7817 <535536456@qq.com>
2018-12-20 17:12:47 +0800

    备份

--- src/components/Screen/index.tsx
+++ src/components/Screen/index.tsx
@@ -2,7 +2,7 @@
  * @Author: lifan
  * @Date: 2018-12-19 21:05:34
  * @Last Modified by: lifan
- * @Last Modified time: 2018-12-20 17:04:39
+ * @Last Modified time: 2018-12-20 17:12:29
  */
 import React, { Component } from 'react';
 import Decorate from '../Decorate';
@@ -18,7 +18,7 @@ interface State {
 }
 
 class Screen extends Component<ScreenProps, State> {
-  private $ref_Panl: React.RefObject<HTMLDivElement> = React.createRef();
+  private readonly $ref_Panl: React.RefObject<HTMLDivElement> = React.createRef();
   public state = {
     w: 0
   }
@@ -37,14 +37,20 @@ class Screen extends Component<ScreenProps, State> {
   }
 
   public render() {
-    // const { matrix } = this.props;
+    const { matrix } = this.props;
 
     return (
       <Decorate>
         <div className={style.wrapper}>
           <div ref={this.$ref_Panl} className={style.panl}>
-            <svg>
-              <Pixel width={12} x={50} y={50} />
+            <svg width="100%" height="100%">
+              {
+                matrix.map((row, index) => (
+                  row.map((b, i) => (
+                    <Pixel key={`${index}_${i}`} width={12} x={50} y={50} highlight={!!b} />
+                  ))
+                ))
+              }
             </svg>
           </div>
         </div>
