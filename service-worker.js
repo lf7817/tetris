bd2091bd49741a2c3a13581d85ec681ae2330b3d
lf7817 <535536456@qq.com>
2019-01-11 22:43:37 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("./precache-manifest.9a55a7ceabe513b9c555c1076f99645b.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("./precache-manifest.954de229fb902cf0581632f009e7bade.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 workbox.clientsClaim();
 
