162effc701450cb36d7275c6f7677d959cc594cf
lf7817 <535536456@qq.com>
2019-01-12 22:13:02 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("./precache-manifest.f9ee2f6c71e57d670d67ff402d57c01c.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("./precache-manifest.954de229fb902cf0581632f009e7bade.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 workbox.clientsClaim();
 
