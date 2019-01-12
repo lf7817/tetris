6341e9be6243bbb3ae6af32bbffa323003352058
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-12 14:14:22 +0000

    Deploy lf7817/tetris to github.com/lf7817/tetris.git:gh-pages

---
+++ precache-manifest.3136f47eaec26ba4c49821133a9d8017.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "271417ba09975e1e1682",
+    "url": "./static/js/main.271417ba.js"
+  },
+  {
+    "revision": "271417ba09975e1e1682",
+    "url": "./static/css/main.7aebcd5c.css"
+  },
+  {
+    "revision": "c727c865982bc12e1ad0a3f53fe1c484",
+    "url": "./index.html"
+  }
+];
