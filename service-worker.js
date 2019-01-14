e563befe333556ec55344ba3f1ff621d5fa05c9a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-14 03:42:39 +0000

    Deploy lf7817/tetris to github.com/lf7817/tetris.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("./precache-manifest.3136f47eaec26ba4c49821133a9d8017.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("./precache-manifest.d1662ef30258e8053d53f1586cc9c1a3.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 workbox.clientsClaim();
 
