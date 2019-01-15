41bd0041622635ad72b91f9660a2894573ac746e
lf7817 <535536456@qq.com>
2019-01-15 15:09:27 +0800

    修复1rem = 20vw Bug

--- src/utils/block.ts
+++ src/utils/block.ts
@@ -2,12 +2,12 @@
  * @Author: lifan
  * @Date: 2019-01-15 10:51:23
  * @Last Modified by: lifan
- * @Last Modified time: 2019-01-15 11:00:47
+ * @Last Modified time: 2019-01-15 11:13:40
  */
 import { BLOCK_SHAPE } from '../constants/block';
 
 /**
- * 随机生成一个方块
+ * 随机生成一个方块类型
  */
 export const getRandomBlockShap = (): BlockShap => {
   const list = Object.keys(BLOCK_SHAPE) as BlockShap[];
