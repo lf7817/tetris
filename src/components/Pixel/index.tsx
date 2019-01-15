db1bc6351459c647b4c16e02eeeb9327d1a56ee4
lf7817 <535536456@qq.com>
2019-01-15 15:47:54 +0800

    优化Pixel组件性能

--- src/components/Pixel/index.tsx
+++ src/components/Pixel/index.tsx
@@ -37,66 +37,74 @@ class Pixel extends Component<PixelProps> {
     const { highlight, x, y, width, hide } = this.props;
     const fillColor = highlight ? style.fillHighlight : style.fillDefault;
     const strokeColor = highlight ? style.strokeHighlight : style.strokeDefault;
-    const isHide = hide ? 0 : 1;
 
     return (
       <Fragment>
         <MediaQuery maxWidth={414}>
-          <g opacity={isHide}>
-            <rect
-              x={x + 1.5}
-              y={y + 1.5}
-              width={width - 3}
-              height={width - 3}
-              style={{ strokeWidth: 1.75, fillOpacity: 0 }}
-              className={strokeColor}
-            />
-            <rect
-              x={x + 3.5}
-              y={y + 3.5}
-              width={width - 7}
-              height={width - 7}
-              className={fillColor}
-            />
-          </g>
+          {
+            !hide ?
+              <g>
+                <rect
+                  x={x + 1.5}
+                  y={y + 1.5}
+                  width={width - 3}
+                  height={width - 3}
+                  style={{ strokeWidth: 1.75, fillOpacity: 0 }}
+                  className={strokeColor}
+                />
+                <rect
+                  x={x + 3.5}
+                  y={y + 3.5}
+                  width={width - 7}
+                  height={width - 7}
+                  className={fillColor}
+                />
+              </g> : null
+          }
         </MediaQuery>
         <MediaQuery minWidth={414} maxWidth={539}>
-          <g opacity={isHide}>
-            <rect
-              x={x + 1.25}
-              y={y + 1.25}
-              width={width - 2.5}
-              height={width - 2.5}
-              style={{ strokeWidth: 1.5, fillOpacity: 0 }}
-              className={strokeColor}
-            />
-            <rect
-              x={x + 3.5}
-              y={y + 3.5}
-              width={width - 7}
-              height={width - 7}
-              className={fillColor}
-            />
-          </g>
+          {
+            !hide ?
+              <g>
+                <rect
+                  x={x + 1.25}
+                  y={y + 1.25}
+                  width={width - 2.5}
+                  height={width - 2.5}
+                  style={{ strokeWidth: 1.5, fillOpacity: 0 }}
+                  className={strokeColor}
+                />
+                <rect
+                  x={x + 3.5}
+                  y={y + 3.5}
+                  width={width - 7}
+                  height={width - 7}
+                  className={fillColor}
+                />
+              </g> : null
+          }
         </MediaQuery>
         <MediaQuery minWidth={540}>
-          <g opacity={isHide}>
-            <rect
-              x={x + 2}
-              y={y + 2}
-              width={width - 4}
-              height={width - 4}
-              style={{ strokeWidth: 2, fillOpacity: 0 }}
-              className={strokeColor}
-            />
-            <rect
-              x={x + 5}
-              y={y + 5}
-              width={width - 10}
-              height={width - 10}
-              className={fillColor}
-            />
-          </g>
+          {
+            !hide ?
+              <g>
+                <rect
+                  x={x + 2}
+                  y={y + 2}
+                  width={width - 4}
+                  height={width - 4}
+                  style={{ strokeWidth: 2, fillOpacity: 0 }}
+                  className={strokeColor}
+                />
+                <rect
+                  x={x + 5}
+                  y={y + 5}
+                  width={width - 10}
+                  height={width - 10}
+                  className={fillColor}
+                />
+              </g> : null
+          }
         </MediaQuery>
       </Fragment>
     );
